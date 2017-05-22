fe5238cb84e6b83f21386401c2bcf816e782941d
Daniel Olivares <daniel.olivares@parkhub.com>
2017-05-22 11:38:45 -0500

    feat(API): initial release
    Initial implementation release

---
+++ __tests__/proto-finder.test.js
@@ -0,0 +1,34 @@
+import path from 'path';
+import protoFinder, { findSingleFile } from '../src';
+
+describe('protoFinder API', () => {
+  test('Should return an instance when default function is called', () => {
+    expect(protoFinder.findSingleFile).toBeDefined();
+    expect(typeof protoFinder.findSingleFile).toBe('function');
+  });
+
+  test('Should expose single findSingleFile function', () => {
+    expect(typeof findSingleFile).toBe('function');
+  });
+});
+
+describe('findSingleFile()', () => {
+  test('Should return the full path to the proto file', async () => {
+    const expectedFilePath = path.resolve(__dirname, './protos/test.proto');
+    const filePath = await findSingleFile('test');
+
+    expect(filePath).toBe(expectedFilePath);
+  });
+
+  test('Should return undefined when file does not exist', async () => {
+    const filePath = await findSingleFile('doesnotexist');
+
+    expect(filePath).toBeUndefined();
+  });
+
+  test('Should return undefined when file name is not passed', async () => {
+    const filePath = await findSingleFile();
+
+    expect(filePath).toBeUndefined();
+  });
+});
---
+++ src/index.js
@@ -0,0 +1,34 @@
+import globby from 'globby';
+
+/**
+ * Find a protofile's full path in a 'protos' directory by searching UP  from your current process.
+ *
+ * @module @parkhub/proto-finder
+ * @author Daniel Olivares
+ *
+ * @example
+ * import protoFinder from '@parkhub/protofinder';
+ *
+ * const protoFullPath = protoFinder.findSingleFile('test');
+ */
+
+/**
+ * Get the full path of a single file by it's name in the nearest 'protos' directory.
+ *
+ * @function findSingleFile
+ * @param String fileName the name of the file to search for
+ * @return {Promise<String, Error>} The full path of the file's location
+ */
+export async function findSingleFile(fileName) {
+  const paths = await globby([`**/protos/${fileName}.proto`], {
+    realpath: true
+  });
+
+  return paths[0];
+}
+
+const protoFinder = {
+  findSingleFile
+};
+
+export default protoFinder;
